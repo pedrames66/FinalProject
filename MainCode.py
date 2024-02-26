095d29086cf3d99a74cd6b40cbc164225a973e50
pedrames <pe.esfahani@gmail.com>
2024-02-26 20:37:05 +0330

    add sum

--- MainCode.py
+++ MainCode.py
@@ -1,10 +1,9 @@
 #Find the sum of all the multiples of 3 or 5 below 1000.
 
 
-
+count = 0
 for i in range(1,10):
-    if i%3 == 0:
-        j = i
-        if j%5 == 0:
-            list1.append(j)
+	if i%3 == 0 or i%5 == 0:
+		
+
 print(list1)
